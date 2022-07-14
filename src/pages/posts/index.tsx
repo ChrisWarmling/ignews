c98b3a6ef35c424938b414a4aef58c8746f26c9b
Christian <christianwarmling@gmail.com>
2022-07-14 00:23:42 -0300

    feat: continuação do modulo Front-end JAMStack

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,38 +1,58 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client'
 import styles from './styles.module.scss';
 
 export default function Posts() {
-  return (
-    <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
 
-        <main className={styles.container}>
-            <div className={styles.posts}>
-                <a href="">
-                    <time>22 de julho de 2022</time>
-                    <strong>Bun (can become) the ideal JavaScript runtime</strong>
-                    <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
-                </a>
-                <a href="">
-                    <time>22 de julho de 2022</time>
-                    <strong>Bun (can become) the ideal JavaScript runtime</strong>
-                    <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
-                </a>
-                <a href="">
-                    <time>22 de julho de 2022</time>
-                    <strong>Bun (can become) the ideal JavaScript runtime</strong>
-                    <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
-                </a>
-                <a href="">
-                    <time>22 de julho de 2022</time>
-                    <strong>Bun (can become) the ideal JavaScript runtime</strong>
-                    <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
-                </a>
-                
-            </div>
-        </main>
-    </>
-  );
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>22 de julho de 2022</time>
+                        <strong>Bun (can become) the ideal JavaScript runtime</strong>
+                        <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
+                    </a>
+                    <a href="">
+                        <time>22 de julho de 2022</time>
+                        <strong>Bun (can become) the ideal JavaScript runtime</strong>
+                        <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
+                    </a>
+                    <a href="">
+                        <time>22 de julho de 2022</time>
+                        <strong>Bun (can become) the ideal JavaScript runtime</strong>
+                        <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
+                    </a>
+                    <a href="">
+                        <time>22 de julho de 2022</time>
+                        <strong>Bun (can become) the ideal JavaScript runtime</strong>
+                        <p>Finally a JS runtime made to perform — running circles around both Deno and Node.js</p>
+                    </a>
+
+                </div>
+            </main>
+        </>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publication')
+    ], {
+        fetch: ['title', 'content'],
+        pageSize: 100,
+    });
+
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+        props: {},
+    };
 }
